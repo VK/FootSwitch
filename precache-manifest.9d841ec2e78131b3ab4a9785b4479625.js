d42853a5b7f9aade4ef3444518860793af768e16
Viktor Krückl <Viktor.Krueckl@osram.com>
2019-08-24 18:24:16 +0200

    initial commit

---
+++ precache-manifest.9d841ec2e78131b3ab4a9785b4479625.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd2877178b196b1129c94bf30f638e8b",
+    "url": "/FootSwitch/img/iconShift.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/FootSwitch/robots.txt"
+  },
+  {
+    "revision": "30600422a978e9641970431aea5f7205",
+    "url": "/FootSwitch/img/iconC.svg"
+  },
+  {
+    "revision": "698eb4f9bf567aa5239a",
+    "url": "/FootSwitch/js/chunk-vendors.28b306d4.js"
+  },
+  {
+    "revision": "c8ca99418b364385b70d",
+    "url": "/FootSwitch/js/app.078469b4.js"
+  },
+  {
+    "revision": "b0d64071774b4d82d5a4ca7f6863807a",
+    "url": "/FootSwitch/index.html"
+  },
+  {
+    "revision": "4cbec5f7d322a5d5136c488f82597033",
+    "url": "/FootSwitch/img/iconRB.svg"
+  },
+  {
+    "revision": "bbb40970cd91cc95cc8feb6d8567cc42",
+    "url": "/FootSwitch/img/iconRA.svg"
+  },
+  {
+    "revision": "0d2cf4cee077faf64a53f445b2607306",
+    "url": "/FootSwitch/img/iconLB.svg"
+  },
+  {
+    "revision": "d7ba5d36773e915e6b9050501c620591",
+    "url": "/FootSwitch/img/iconLA.svg"
+  },
+  {
+    "revision": "23c5665b4b4eb354670bd307d143d358",
+    "url": "/FootSwitch/img/iconCtr.svg"
+  },
+  {
+    "revision": "6c9763f103d21927b847b1fc286b3533",
+    "url": "/FootSwitch/img/iconAlt.svg"
+  },
+  {
+    "revision": "c8ca99418b364385b70d",
+    "url": "/FootSwitch/css/app.dc9b57b7.css"
+  }
+];
